ea2408608dd93089d0053f9a6ad12fd24557edf7
Kelvin <kelvinosagie05@gmail.com>
2025-10-30 21:01:55 +0000

    Added in a comment for the comparisons

--- bubble_sort.py
+++ bubble_sort.py
@@ -11,7 +11,7 @@ def bubble_sort(arr):
             c+=1
             a_i += 2
             if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j] ; a+=3 ; a_i+=4
+                arr[j], arr[j + 1] = arr[j + 1], arr[j] ; a+=3 ; a_i+=4 #Counting the assignments and indexing
                 swapped = True
         if not swapped:
             break
@@ -21,4 +21,4 @@ n = 50
 arr = [random.randint(1, 100) for _ in range(n)]
 
 sorted_arr, x,c, a_i = bubble_sort(arr)
-print(x, "Assignments and", c, "Comparisons", a_i, "Array Indexs'")
+print(x, "Assignments and", c, "Comparisons", a_i, "Array Index'")
