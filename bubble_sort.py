74ed5edade687a98d91ca9159c93bfddab273af5
Kelvin <kelvinosagie05@gmail.com>
2025-11-06 11:05:40 +0000

    Created a hardcoded function for the bubble checks comparison and index

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,17 +1,28 @@
 import random
 
+a_i = 0  # global counter
+c = 0 # Comparisons
+a = 0  # Assignments
+
+def count(x, ai_inc=2, c_inc=1):
+    global a_i
+    global c
+    c += c_inc
+    a_i += ai_inc
+    return x
+
 def bubble_sort(arr):
     n = len(arr)
-    a = 0 # Assignments
-    c = 0 # Comparisons
-    a_i = 0 # Array Indexing
+    global a_i  # Array Indexing
+    global a
+    global c
     for i in range(n):
         swapped = False
         for j in range(0, n - i - 1):
-            c+=1
-            a_i += 2
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j] ; a+=3 ; a_i+=4 #Counting the assignments and indexing
+            if count(arr[j] > arr[j + 1]):
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                a += 3
+                a_i += 4  # for index ops
                 swapped = True
         if not swapped:
             break
@@ -20,5 +31,5 @@ def bubble_sort(arr):
 n = 50
 arr = [random.randint(1, 100) for _ in range(n)]
 
-sorted_arr, x,c, a_i = bubble_sort(arr)
-print(x, "Assignments and", c, "Comparisons", a_i, "Array Index'")
+sorted_arr, a, c, a_i = bubble_sort(arr)
+print(a, "Assignments and", c, "Comparisons", a_i, "Array Indexes")
