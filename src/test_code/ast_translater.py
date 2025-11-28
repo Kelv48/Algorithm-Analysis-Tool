1fcf2ac4f41f390606aa532788cd57450f2510fd
Kelvin <kelvinosagie05@gmail.com>
2025-11-28 12:04:48 +0000

    Removed the test code directory after the completion of its objective

--- src/test_code/ast_translater.py
+++
@@ -1,6 +0,0 @@
-import ast
-import operator
-
-# Transform asts from source code of a different language into python asts
-# For example, converting a simple arithmetic expression language into python asts
-# This would allow the analysis tool to analyze algorithms written in that language
